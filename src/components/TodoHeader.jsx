8801a2e4e492f3ff2e3bfecc99cd9975cae78b55
김안나 <yorocobe@gmail.com>
2022-09-15 17:03:16 +0900

    feat: 리스트 header 날짜 관련 기능 구현

--- src/components/TodoHeader.jsx
+++ src/components/TodoHeader.jsx
@@ -1,10 +1,46 @@
 import React from "react";
 
 function TodoHeader() {
+  let now = new Date();
+  let currentDate = `${now.getFullYear()}년 ${now.getMonth()}월 ${now.getDate()}일`;
+  let getDay = "";
+  switch (now.getDay()) {
+    case 0: {
+      getDay = "일요일";
+      break;
+    }
+    case 1: {
+      getDay = "월요일";
+      break;
+    }
+    case 2: {
+      getDay = "화요일";
+      break;
+    }
+    case 3: {
+      getDay = "수요일";
+      break;
+    }
+    case 4: {
+      getDay = "목요일";
+      break;
+    }
+    case 5: {
+      getDay = "금요일";
+      break;
+    }
+    case 6: {
+      getDay = "토요일";
+      break;
+    }
+    default:
+      break;
+  }
+
   return (
     <header>
-      <h1 className="fs-large">2022년 9월 2일</h1>
-      <h3 className="fs-medium">수요일</h3>
+      <h1 className="fs-large">{currentDate}</h1>
+      <h3 className="fs-medium">{getDay}</h3>
       <span>할 일 n개 남음</span>
     </header>
   );
