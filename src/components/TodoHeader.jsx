614d6c3322ce39de5ba8a92c35d47e07111e767c
김안나 <yorocobe@gmail.com>
2022-09-16 13:45:24 +0900

    feat: 날짜 부분 코드 리팩토링 및 할 일 갯수 띄우기

--- src/components/TodoHeader.jsx
+++ src/components/TodoHeader.jsx
@@ -3,49 +3,21 @@ import { useTodoState } from "../TodoContext";
 
 function TodoHeader() {
   const todos = useTodoState();
-  console.log(todos);
+  const undoneTodos = todos.filter((todo) => !todo.done);
 
   let now = new Date();
-  let currentDate = `${now.getFullYear()}년 ${now.getMonth()}월 ${now.getDate()}일`;
-  let getDay = "";
-  switch (now.getDay()) {
-    case 0: {
-      getDay = "일요일";
-      break;
-    }
-    case 1: {
-      getDay = "월요일";
-      break;
-    }
-    case 2: {
-      getDay = "화요일";
-      break;
-    }
-    case 3: {
-      getDay = "수요일";
-      break;
-    }
-    case 4: {
-      getDay = "목요일";
-      break;
-    }
-    case 5: {
-      getDay = "금요일";
-      break;
-    }
-    case 6: {
-      getDay = "토요일";
-      break;
-    }
-    default:
-      break;
-  }
+  const dateString = now.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const dayName = now.toLocaleDateString("ko-KR", { weekday: "long" });
 
   return (
     <header>
-      <h1 className="fs-large">{currentDate}</h1>
-      <h3 className="fs-medium">{getDay}</h3>
-      <span>할 일 n개 남음</span>
+      <h1 className="fs-large">{dateString}</h1>
+      <h3 className="fs-medium">{dayName}</h3>
+      <span>할 일 {undoneTodos.length}개 남음</span>
     </header>
   );
 }
